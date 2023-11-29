02ddb9e21a13d06c20138c79231f01b2ee116cf3
Alexânia <alexaniagiusti@gmail.com>
2023-11-29 16:44:30 -0300

    adição de condicional que compara se o número inserido corresponde ao número secreto

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert ('Boas vindas ao jogo do número secreto');
 let secretNumber = 29;
-prompt('Escolha um número entre 1 e 30');
+let shot = prompt('Escolha um número entre 1 e 30');
+
+if (secretNumber == shot) {
+    console.log('Isso aí! Você descobriu o número secreto (29)');
+}
